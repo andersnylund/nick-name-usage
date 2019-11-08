6eccb12693eeed96cc1b8ce155e4c88046aee070
Anders Nylund <anders@andersnylund.com>
2019-11-08 22:25:09 +0200

    Some configuration

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,36 @@ const Container = styled.section`
 
 const App: React.FC = () => {
   const [name, setName] = useState('');
-  const changeHandler = (event: ChangeEvent<HTMLInputElement>) =>
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>): void =>
     setName(event.target.value);
 
   const nicknames = nicks(name);
 
   return (
     <Container>
-      <label>
-        Your name
-        <input type="text" value={name} onChange={changeHandler}></input>
-      </label>
-      <ul>
+      <Label>
+        <span>Your name</span>
+        <input type="text" value={name} onChange={changeHandler} />
+      </Label>
+      <List>
         {nicknames.map(nick => (
           <li key={nick}>{nick}</li>
         ))}
-      </ul>
+      </List>
     </Container>
   );
 };
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+
+const List = styled.ul`
+  min-height: 200px;
+  list-style: none;
+  margin: 0;
+  padding: 10px 0 0 0;
+`;
 
 export default App;
---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+import 'typeface-roboto';
+
+export default createGlobalStyle`
+  html {
+    font-family: Roboto;
+  }
+`;
